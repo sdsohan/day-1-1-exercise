2ff9e5f1623ce339d8ec37bd8d44b45c86011f25
Syed Sohan Ahmed <sdsohan48@gmail.com>
2021-03-15 18:26:32 +0000

    f

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 print('Welcome to Brand Name Generator'"\n")
 name = input('Enter your name: ')
-age = int(input('Enter your age: '))
+age = float(input('Enter your age: '))
 homeTown = input('Enter your home town name: ')
 
 print(f"{name} is your name \n{age} is your age \n{homeTown} is your Home Town ")
